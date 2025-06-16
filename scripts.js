d999fe049eb4a31d4c1a7fbb72b6f291ec2dd6b3
Miguel <migueljm1008@gmail.com>
2025-06-16 17:17:50 +0200

    Ej 1-6 terminados

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log(document.title);
+//Ej 1
+document.getElementById('gen-1').innerHTML = 'Generasión 1 Pokimon';
+//Ej 2
+document.getElementsByClassName('infocard-list infocard-list-pkmn-lg')[0].style.backgroundColor = 'green';
+//Ej 3 
+console.log(window.location.href);
+//Ej 4
+console.log(window.location.host) 
+//Ej 5
+console.log(document.images);
+//Ej 6
+let imgs = document.images; 
+for (let i= 0; i<imgs.length; i++) {
+    imgs[i].src= "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+}
+//Ej 7
