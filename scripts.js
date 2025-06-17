b62e5695f62289c51aa911182b4d291c5067790f
Miguel <migueljm1008@gmail.com>
2025-06-17 10:26:41 +0200

    mas codigo

--- scripts.js
+++ scripts.js
@@ -8,9 +8,21 @@ console.log(window.location.href);
 console.log(window.location.host) 
 //Ej 5
 console.log(document.images);
+
+/*
+let  imgs = document.querySelectorAll('img-fixed img-sprite');
+for(let i= 0; i< imgs.length; i++) {
+    console.log(imgs[i]);
+}
+*/
 //Ej 6
 let imgs = document.images; 
 for (let i= 0; i<imgs.length; i++) {
     imgs[i].src= "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
 }
+/*
+let img = document.querySelectorAll('img');
+for (let i = 0; i< img.length; i++) {
+    img[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+} */
 //Ej 7
